77018c32e5c953ccf98a520031dbfdeaf6094ffd
Cixtor <cixtords@gmail.com>
2017-01-02 09:00:00 -0600

    Add missing documentation in all defined functions

--- logging.go
+++ logging.go
@@ -8,6 +8,13 @@ import (
 	"time"
 )
 
+// LoggingHandler implements the default HTTP handler interface.
+type LoggingHandler struct {
+	handler http.Handler
+	out     io.Writer
+}
+
+// LogRecord defines which data will be collected from the requests.
 type LogRecord struct {
 	http.ResponseWriter
 
@@ -21,6 +28,12 @@ type LogRecord struct {
 	elapsedTime   time.Duration
 }
 
+// NewLoggingHandler creates an instance of the HTTP request catcher.
+func NewLoggingHandler(handler http.Handler, out io.Writer) http.Handler {
+	return &LoggingHandler{handler, out}
+}
+
+// Log sends the access log string to the available writer.
 func (r *LogRecord) Log(out io.Writer) {
 	timeFormatted := r.time.Format("02/Jan/2006 03:04:05")
 	requestLine := fmt.Sprintf("%s %s %s", r.method, r.uri, r.protocol)
@@ -35,26 +48,24 @@ func (r *LogRecord) Log(out io.Writer) {
 		r.elapsedTime.Seconds())
 }
 
+// Write writes the data to the connection as part of an HTTP reply.
 func (r *LogRecord) Write(p []byte) (int, error) {
 	written, err := r.ResponseWriter.Write(p)
 	r.responseBytes += int64(written)
 	return written, err
 }
 
+// WriteHeader sends an HTTP response header with status code.
+// If WriteHeader is not called explicitly, the first call to Write
+// will trigger an implicit WriteHeader(http.StatusOK).
+// Thus explicit calls to WriteHeader are mainly used to
+// send error codes.
 func (r *LogRecord) WriteHeader(status int) {
 	r.status = status
 	r.ResponseWriter.WriteHeader(status)
 }
 
-type LoggingHandler struct {
-	handler http.Handler
-	out     io.Writer
-}
-
-func NewLoggingHandler(handler http.Handler, out io.Writer) http.Handler {
-	return &LoggingHandler{handler, out}
-}
-
+// ServeHTTP calls f(w, r).
 func (h *LoggingHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	clientIP := r.RemoteAddr
 
